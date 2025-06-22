6dc585acd61a8cbc33d0451d87d6ab300d116071
junjeong <wn8624@naver.com>
2025-06-22 16:52:31 +0900

    feat: 회원가입 페이지도 validation 완성

--- src/components/button/AuthButton.tsx
+++ src/components/button/AuthButton.tsx
@@ -1,15 +1,13 @@
 interface AuthButtonProps {
   type: "로그인" | "회원가입";
-  handleSubmit: () => void;
 }
 
-const AuthButton = ({ type = "로그인", handleSubmit }: AuthButtonProps) => {
+const AuthButton = ({ type = "로그인" }: AuthButtonProps) => {
   const isType = type === "로그인" ? "로그인" : "회원가입";
 
   return (
     <button
       type="submit"
-      onClick={handleSubmit}
       className="mt-4 w-full h-14 rounded-full flex justify-center bg-[#9CA3AF] text-lg font-semibold items-center text-white ative:bg-[#3b82f6] hover:bg-[#3b82f6] cursor-pointer"
     >
       {isType}
