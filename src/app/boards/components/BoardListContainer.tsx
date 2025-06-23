4d634198bb742e828b856db871b6802c5eb43752
junjeong <wn8624@naver.com>
2025-06-23 23:35:53 +0900

    ref: 게시판 페이지 ui 전면 수정

--- src/app/boards/components/BoardListContainer.tsx
+++ src/app/boards/components/BoardListContainer.tsx
@@ -1,16 +1,16 @@
 import Title from "@/app/boards/components/Title";
-import CategoryList from "@/app/boards/components/CategoryList";
 import SearchArticleBar from "@/app/boards/components/SearchArticleBar";
 import BoardItemList from "@/app/boards/components/BoardItemList";
 
 const BoardListContainer = () => {
   return (
-    <div className="flex flex-col gap-6 w-full max-w-[1200px]">
-      <Title />
-      <CategoryList />
-      <SearchArticleBar />
+    <section className="flex flex-col gap-6 w-full max-w-[1200px]">
+      <div className="flex items-center">
+        <Title content="전체 목록" />
+        <SearchArticleBar />
+      </div>
       <BoardItemList />
-    </div>
+    </section>
   );
 };
 
---
+++ src/util/isoStringToTime.ts
@@ -0,0 +1,11 @@
+const isoStringToTime = (isoString: string) => {
+  const date = new Date(isoString);
+  const yyyy = date.getFullYear();
+  const mm = String(date.getMonth() + 1).padStart(2, "0");
+  const dd = String(date.getDate()).padStart(2, "0");
+  const hh = String(date.getHours()).padStart(2, "0");
+  const mi = String(date.getMinutes()).padStart(2, "0");
+  return `${yyyy}.${mm}.${dd} ${hh}:${mi}`;
+};
+
+export default isoStringToTime;
